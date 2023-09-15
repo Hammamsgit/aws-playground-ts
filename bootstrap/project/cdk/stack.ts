7f3007f70ac0902d13879e372ea6417c96adb816
hammamsgit <hammamalzilitni@cinchs-MacBook-Pro-2.local>
2023-09-15 15:23:10 +0100

    again

--- bootstrap/project/cdk/stack.ts
+++ bootstrap/project/cdk/stack.ts
@@ -11,38 +11,6 @@ export class PlaygroundStack extends cdk.Stack {
 
     const { buildName } = props
 
-    const provider = new OpenIdConnectProvider(this, "MyProvider", {
-      url: "https://token.actions.githubusercontent.com",
-      clientIds: ["sts.amazonaws.com"],
-    })
-
-    const GitHubPrincipal = new OpenIdConnectPrincipal(provider).withConditions({
-      StringLike: {
-        "token.actions.githubusercontent.com:sub": `repo:hammamsgit/aws-playground-ts:*`,
-      },
-    })
-
-    new Role(this, "GitHubActionsRole", {
-      assumedBy: GitHubPrincipal,
-      description:
-        "Role assumed by GitHubPrincipal for deploying from CI using aws cdk",
-      roleName: "github-ci-role",
-      maxSessionDuration: cdk.Duration.hours(1),
-      inlinePolicies: {
-        CdkDeploymentPolicy: new PolicyDocument({
-          assignSids: true,
-          statements: [
-            new PolicyStatement({
-              effect: Effect.ALLOW,
-              actions: ["sts:AssumeRole"],
-              resources: [`arn:aws:iam::${this.account}:role/cdk-*`],
-            }),
-          ],
-        }),
-      },
-    })
-
-
     new NodejsFunction(this, 'HelloWorld', {
       functionName: buildName('testing-lambda'),
       entry: path.join(__dirname, '../src/hello-world.ts'),
