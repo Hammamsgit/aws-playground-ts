29c49b61b450b4c2e6e590062b0dbafc65fe519f
hammamsgit <hammamalzilitni@cinchs-MacBook-Pro-2.local>
2023-09-22 16:03:07 +0100

    again

--- bin/aws-learning-project.ts
+++ bin/aws-learning-project.ts
@@ -4,9 +4,9 @@ import * as bootstrap from '../bootstrap/project/cdk/stack'
 
 const app = new cdk.App()
 
-const branchName = app.node.tryGetContext('branchName')
-const stackName = app.node.tryGetContext('stackName')
-const playgroundIdentity = app.node.tryGetContext('playgroundIdentity')
+const branchName = app.node.tryGetContext('branchName') || process.env.BRANCH_NAME
+const stackName = app.node.tryGetContext('stackName') || process.env.STACK_NAME
+const playgroundIdentity = app.node.tryGetContext('playgroundIdentity')  || process.env.PLAYGROUND_IDENTITY
 
 const playgroundPath = branchName.split('/').splice(1, 2).join('/')
 const stackPath = path.join(__dirname, '../playground', playgroundPath, 'cdk', 'stack.ts')
